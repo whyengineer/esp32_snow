57b702c255f237d3358af32475f13e0f90c1cfb3
fzeng <fzeng@cam2cloud.com>
2018-01-12 10:35:07 +0800

    add 3d show

---
+++ example/3d_show/main/main.c
@@ -0,0 +1,99 @@
+/* GPIO Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+#include "wm8978.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdmmc_defs.h"
+#include "sdmmc_cmd.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include <sys/socket.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "eth.h"
+#include "event.h"
+#include "wifi.h"
+#include "hal_i2c.h"
+#include "hal_i2s.h"
+#include "wm8978.h"
+#include "webserver.h"
+#include "http.h"
+#include "cJSON.h"
+#include "mdns_task.h"
+#include "audio.h"
+#include <dirent.h>
+#include "esp_heap_caps.h"
+#include "euler.h"
+#include "websocket.h"
+#include "esp_heap_caps.h"
+
+
+#define TAG "main:"
+// typedef int (*http_data_cb) (http_parser*, const char *at, size_t length);
+// typedef int (*http_cb) (http_parser*);
+
+
+//char* http_body;
+
+#define GPIO_OUTPUT_IO_0    5
+#define GPIO_OUTPUT_PIN_SEL  ((1<<GPIO_OUTPUT_IO_0))
+
+void app_main()
+{
+    esp_err_t err;
+    event_engine_init();
+    nvs_flash_init();
+    tcpip_adapter_init();
+    wifi_init_sta("Transee21_TP1","02197545");
+    //wifi_init_softap("we","1234567890");
+    /*init gpio*/
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+    gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+    /*init i2c*/
+    hal_i2c_init(0,19,18);
+
+    //wait got ip address
+     xEventGroupWaitBits(station_event_group,STA_GOTIP_BIT,pdTRUE,pdTRUE,portMAX_DELAY);
+    //print ip address
+    tcpip_adapter_ip_info_t ip;
+    memset(&ip, 0, sizeof(tcpip_adapter_ip_info_t));
+    if (tcpip_adapter_get_ip_info(ESP_IF_WIFI_STA, &ip) == 0) {
+        ESP_LOGI(TAG, "~~~~~~~~~~~");
+        ESP_LOGI(TAG, "ETHIP:"IPSTR, IP2STR(&ip.ip));
+        ESP_LOGI(TAG, "ETHPMASK:"IPSTR, IP2STR(&ip.netmask));
+        ESP_LOGI(TAG, "ETHPGW:"IPSTR, IP2STR(&ip.gw));
+        ESP_LOGI(TAG, "~~~~~~~~~~~");
+    }
+    /*start websocket server*/
+    xTaskCreate(ws_server, "web_socket_task", 4096, NULL, +6, NULL);
+    /*start euler task*/
+    xTaskCreate(euler_task, "euler_task", 4096*2, NULL, +6, NULL);
+    /*print the last ram*/
+    size_t free8start=heap_caps_get_free_size(MALLOC_CAP_8BIT);
+    size_t free32start=heap_caps_get_free_size(MALLOC_CAP_32BIT);
+    ESP_LOGI(TAG,"free mem8bit: %d mem32bit: %d\n",free8start,free32start);
+
+    vTaskSuspend(NULL); 
+    while(1){
+    }
+}
+
